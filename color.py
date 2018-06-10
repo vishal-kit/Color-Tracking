08b89b3b2f45c8ad3958b6fdddeb05621b801b11
vishal-kit <vishal.kit@outlook.com>
2018-06-10 20:56:53 +0900

    fixed for green

--- color.py
+++ color.py
@@ -1,6 +1,6 @@
 #importing modules
 
-import cv2   
+import cv2
 import numpy as np
 
 #capturing video through webcam
@@ -8,7 +8,7 @@ cap=cv2.VideoCapture(0)
 
 while(1):
 	_, img = cap.read()
-	    
+
 	#converting frame(img i.e BGR) to HSV (hue-saturation-value)
 
 	hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
@@ -20,17 +20,22 @@ while(1):
 	#defining the Range of Blue color
 	blue_lower=np.array([99,115,150],np.uint8)
 	blue_upper=np.array([110,255,255],np.uint8)
-	
+
 	#defining the Range of yellow color
 	yellow_lower=np.array([22,60,200],np.uint8)
 	yellow_upper=np.array([60,255,255],np.uint8)
+
+	#defining the Range of Green color
+    green_lower=np.array([65,60,60],np.uint8)
+    green_upper=np.array([80,255,255],np.uint8)
 
 	#finding the range of red,blue and yellow color in the image
 	red=cv2.inRange(hsv, red_lower, red_upper)
 	blue=cv2.inRange(hsv,blue_lower,blue_upper)
 	yellow=cv2.inRange(hsv,yellow_lower,yellow_upper)
-	
-	#Morphological transformation, Dilation  	
+	green=cv2.inRange(hsv,green_lower,green_upper)
+
+	#Morphological transformation, Dilation
 	kernal = np.ones((5 ,5), "uint8")
 
         red=cv2.dilate(red, kernal)
@@ -40,26 +45,28 @@ while(1):
 	res1=cv2.bitwise_and(img, img, mask = blue)
 
 	yellow=cv2.dilate(yellow,kernal)
-	res2=cv2.bitwise_and(img, img, mask = yellow)    
+	res2=cv2.bitwise_and(img, img, mask = yellow)
 
+	green=cv2.dilate(green,kernal)
+    res3=cv2.bitwise_and(img, img, mask = green)
 
 	#Tracking the Red Color
 	(_,contours,hierarchy)=cv2.findContours(red,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-	
+
 	for pic, contour in enumerate(contours):
 		area = cv2.contourArea(contour)
 		if(area>300):
-			
-			x,y,w,h = cv2.boundingRect(contour)	
+
+			x,y,w,h = cv2.boundingRect(contour)
 			img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
 			cv2.putText(img,"RED color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255))
-			
+
 	#Tracking the Blue Color
 	(_,contours,hierarchy)=cv2.findContours(blue,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	for pic, contour in enumerate(contours):
 		area = cv2.contourArea(contour)
 		if(area>300):
-			x,y,w,h = cv2.boundingRect(contour)	
+			x,y,w,h = cv2.boundingRect(contour)
 			img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 			cv2.putText(img,"Blue color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
 
@@ -68,18 +75,24 @@ while(1):
 	for pic, contour in enumerate(contours):
 		area = cv2.contourArea(contour)
 		if(area>300):
-			x,y,w,h = cv2.boundingRect(contour)	
-			img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-			cv2.putText(img,"yellow  color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,0))  
-            
-           
+			x,y,w,h = cv2.boundingRect(contour)
+			img = cv2.rectangle(img,(x,y),(x+w,y+h),(60,255,255),2)
+			cv2.putText(img,"yellow  color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (60,255,255))
+
+	#Tracking the green Color
+    (_,contours,hierarchy)=cv2.findContours(green,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area>300):
+            x,y,w,h = cv2.boundingRect(contour)
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(img,"green  color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,0))
+
+
     	#cv2.imshow("Redcolour",red)
     	cv2.imshow("Color Tracking",img)
-    	#cv2.imshow("red",res) 	
+    	#cv2.imshow("red",res)
     	if cv2.waitKey(10) & 0xFF == ord('q'):
     		cap.release()
     		cv2.destroyAllWindows()
-    		break  
-          
-
-    
+    		break
